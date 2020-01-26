f8fc9ad7189a25315742b1e2a2b282fc8205055e
Terence Mah <terence.mcj@gmail.com>
2020-01-26 21:15:12 +0800

    iP Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,25 +8,37 @@ public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<String> list = new ArrayList<>(100);
+        ArrayList<Task> list = new ArrayList<>(100);
 
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
         String command = br.readLine();
+        String[] input = command.split(" ", 2);
 
-        while (!command.equals("bye")) {
-            switch (command) {
+        while (!input[0].equals("bye")) {
+            switch (input[0]) {
                 case "list":
+                    System.out.println("    Here are the tasks in your list:");
                     int listLength = list.size();
                     for (int i = 0; i < listLength; i++) {
-                        System.out.println("    " + (i + 1) + ". " + list.get(i));
+                        System.out.println("    " + (i + 1) + ".[" + list.get(i).getStatusIcon()
+                                + "] " + list.get(i).getName());
                     }
                     break;
+                case "done":
+                    int index = Integer.parseInt(input[1]) - 1;
+                    list.get(index).setDone();
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("      [" + list.get(index).getStatusIcon()
+                            + "] " + list.get(index).getName());
+                    break;
                 default:
-                    list.add(command);
+                    list.add(new Task(command));
                     System.out.println("    added: " + command);
             }
+
             command = br.readLine();
+            input = command.split(" ", 2);
         }
 
         System.out.println("    Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    protected boolean isDone;
+    protected String name;
+
+    public Task(String name) {
+        this.name = name;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+    public String getName() {
+        return name;
+    }
+    public void setDone() {
+        isDone = true;
+    }
+}

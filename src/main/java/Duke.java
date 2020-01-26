4ca0c88f70ff93707dc9eaee2887f423e28f62e9
Terence Mah <terence.mcj@gmail.com>
2020-01-26 18:55:29 +0800

    iP Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Duke {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<>(100);
+
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
         String command = br.readLine();
 
         while (!command.equals("bye")) {
-            System.out.println("    " + command);
+            switch (command) {
+                case "list":
+                    int listLength = list.size();
+                    for (int i = 0; i < listLength; i++) {
+                        System.out.println("    " + (i + 1) + ". " + list.get(i));
+                    }
+                    break;
+                default:
+                    list.add(command);
+                    System.out.println("    added: " + command);
+            }
             command = br.readLine();
         }
 

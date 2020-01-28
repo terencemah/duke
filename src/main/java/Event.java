f65c56b616e7b7e5f5a19da87639f0c598b8c9e3
Terence Mah <terence.mcj@gmail.com>
2020-01-28 15:08:42 +0800

    iP Level-7

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,15 @@
 public class Event extends Task {
-    protected String time;
+    protected String eventTime;
 
-    public Event(String name, String time) {
+    public Event(String name, String eventTime) {
         super(name);
-        this.time = time;
+        this.eventTime = eventTime;
     }
 
     public String getTaskDisplay() {
-        return "[E][" + getStatusIcon() + "] " + name + " (at: " + time + ")";
+        return "[E][" + getStatusIcon() + "] " + name + " (at: " + eventTime + ")";
     }
     public String getSaveDisplay() {
-        return "E|" + (isDone ? "1" : "0") + "|" + name + "|" + time + "\n";
+        return "E|" + (isDone ? "1" : "0") + "|" + name + "|" + eventTime + "\n";
     }
 }

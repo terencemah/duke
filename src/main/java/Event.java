6271c66ef759dba1e77266e52f8d8c10d1e37381
Terence Mah <terence.mcj@gmail.com>
2020-01-28 15:34:53 +0800

    iP Level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,54 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.StringTokenizer;
+
 public class Event extends Task {
+<<<<<<< Updated upstream
     protected String time;
 
     public Event(String name, String time) {
         super(name);
         this.time = time;
+=======
+    protected String eventTime;
+    protected LocalDate date;
+    protected LocalTime time;
+
+    public Event(String name, String input) {
+        super(name);
+
+        StringTokenizer st = new StringTokenizer(input);
+        String dateString = st.nextToken();
+        try {
+            date = LocalDate.parse(dateString);
+        } catch (DateTimeParseException e) {
+            try {
+                date = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("d/M/yyyy"));
+            } catch (DateTimeParseException x) {
+                // do nothing
+            }
+        }
+
+        if (st.hasMoreTokens()) {
+            String timeString = st.nextToken();
+            try {
+                time = LocalTime.parse(timeString, DateTimeFormatter.ofPattern("HHmm"));
+            } catch (DateTimeParseException e) {
+                // do nothing
+            }
+        }
+
+        if (date != null) {
+            eventTime = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+            if (time != null) {
+                eventTime = eventTime + " " + time.format(DateTimeFormatter.ofPattern("K.mma"));
+            }
+        } else {
+            eventTime = input;
+        }
+>>>>>>> Stashed changes
     }
 
     public String getTaskDisplay() {
